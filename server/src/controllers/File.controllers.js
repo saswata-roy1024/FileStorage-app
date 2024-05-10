6a3de1490c2c05270fb326ac9d722cca6a61a566
Saswata Roy <saswata.roy1024@gmail.com>
2024-05-11 01:13:46 +0530

    Server: Uploading files to cloudinary and storing file information in mongodb.

--- server/src/controllers/File.controllers.js
+++ server/src/controllers/File.controllers.js
@@ -1,8 +1,49 @@
-
+import cloudinary from "../utils/cloudinary.js";
+import File from "../models/file.models.js"
+import fs from 'fs';
+import { log } from "console";
 
 const Upload = (req, res) => {
     console.log(req.file);
-    res.status(200).send("ok")
+    cloudinary.uploader.upload(req.file.path, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                success: false,
+                message: "Error"
+            })
+        } else {
+            File.create({
+                userId: "0000",
+                filename: result.original_filename,
+                url: result.secure_url,
+                type: result.resource_type,
+                format: result.format,
+                size: result.bytes,
+            }).then(() => {
+                fs.unlink(`uploads/${req.file.filename}`, function (err) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+
+                        console.log('File deleted!');
+                    }
+                });
+                res.status(200).json({
+                    success: true,
+                    message: "Uploaded!",
+                })
+            })
+            console.log(result);
+        }
+    })
 }
+
+
+
+
+
+
+
 
 export { Upload }
