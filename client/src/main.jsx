577d2c76538e8bacf09574e2010f277991906cf3
Saswata Roy <saswata.roy1024@gmail.com>
2024-05-27 01:15:50 +0530

    Client: Axios Setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import axios from 'axios'
 import App from './App.jsx'
 import './index.css'
+
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.withCredentials = true;
+axios.interceptors.response.use(response => {
+  console.log(response)  // handels axios respones and error response
+  return response;
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
