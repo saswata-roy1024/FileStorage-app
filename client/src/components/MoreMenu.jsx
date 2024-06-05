a44c831ba622bb530760d2cdc10a02a0daf4403f
Saswata Roy <saswata.roy1024@gmail.com>
2024-06-05 16:52:14 +0530

    Client: Adding Share file feature in MoreMenu.

--- client/src/components/MoreMenu.jsx
+++ client/src/components/MoreMenu.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import downloadFile from './features/downloadFile';
 import { toggleStarFile, deleteFile } from '@/Redux/Slices/filesSlice';
-import { EllipsisVertical } from 'lucide-react';
+import { EllipsisVertical, Copy } from 'lucide-react';
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -11,8 +11,23 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+import { Input } from './ui/input';
+import { Button } from './ui/button';
 
 function MoreMenu({ file }) {
+
+    const [isAlertDialogOpen, setIsAlertDialogOpen] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -29,7 +44,11 @@ function MoreMenu({ file }) {
         downloadFile(file.url, file.filename)
     };
 
-    return (
+    const handleCopyLink = () => {
+        navigator.clipboard.writeText(file.url);
+    };
+
+    return (<>
 
         <DropdownMenu>
             <DropdownMenuTrigger className="absolute top-2 right-2 p-1 rounded-sm border bg-opacity-75 hover:bg-gray-200" >
@@ -37,7 +56,7 @@ function MoreMenu({ file }) {
             </DropdownMenuTrigger>
             <DropdownMenuContent className='px-2 *:text-base *:font-semibold *:w-36'>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem >Share</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setIsAlertDialogOpen(true)} >Share</DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem onClick={handleDownloadFile}>Download</DropdownMenuItem>
                 <DropdownMenuSeparator />
@@ -48,7 +67,28 @@ function MoreMenu({ file }) {
             </DropdownMenuContent>
         </DropdownMenu>
 
-    )
+
+        <AlertDialog open={isAlertDialogOpen}>
+            <AlertDialogTrigger asChild>
+                <button className="hidden">Open</button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Link:</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        <div className="flex items-center space-x-2">
+                            <Input value={file.url} disabled className='disabled:cursor-default disabled:opacity-100'/>
+                            <Button variant="outline" size="icon" onClick={handleCopyLink}><Copy /></Button>
+                        </div>
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogAction onClick={() => setIsAlertDialogOpen(false)}>Done</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+
+    </>)
 }
 
 export default MoreMenu
