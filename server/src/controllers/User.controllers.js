a4561ed630a33a8fe81914357f3d6b0e69c654b3
Saswata Roy <saswata.roy1024@gmail.com>
2024-06-08 22:17:37 +0530

    Server: Setup Verify Otp functionaly.

--- server/src/controllers/User.controllers.js
+++ server/src/controllers/User.controllers.js
@@ -99,9 +99,32 @@ const sendOtp = async (req, res) => {
     }
 };
 
-const verifyOtp = (req, res) => {
 
-}
+
+const verifyOtp = async (req, res) => {
+    const _id = req.session.passport.user;
+    if (!_id) return res.status(401).send('Unauthorized');
+
+    const { otp } = req.body;
+    if (!otp) return res.status(400).send('Bad Request');
+
+    try {
+        const otpRecord = await otpModel.findOne({ userId: _id });
+        console.log(_id);
+        console.log(otpRecord);
+
+        if (!otpRecord) return res.status(404).send('OTP not found');
+
+        const isMatch = await bcrypt.compare(otp, otpRecord.otp);
+        if (!isMatch) return res.status(400).send('Invalid OTP');
+
+        res.send('OTP verified successfully');
+    } catch (error) {
+        console.log('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
 
 
 export { fetchUser, updateUser, sendOtp, verifyOtp };
