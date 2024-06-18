98eedd540f02edb5330cf085cbdafd802374bb39
Saswata Roy <saswata.roy1024@gmail.com>
2024-06-19 00:07:01 +0530

    Client: Adding feature to display saved files in the CardContainer.

--- client/src/components/CardContainer.jsx
+++ client/src/components/CardContainer.jsx
@@ -1,55 +1,67 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchFiles } from '@/Redux/Slices/filesSlice';
+import { fetchSaves } from '@/Redux/Slices/savesSlice';
 import Card from './Card';
 
 function CardContainer() {
-  const search = useSelector((state) => state.search.value);
-  const dispatch = useDispatch();
-  const files = useSelector((state) => state.files.files);
-  const selectedOptions = useSelector((state) => state.dropdown.selectedOptions);
-  const tabs = useSelector((state) => state.tabs.value);
-  const sortBy = useSelector((state) => state.sortBy.value);
+    const search = useSelector((state) => state.search.value);
+    const dispatch = useDispatch();
+    const files = useSelector((state) => state.files.files);
+    const selectedOptions = useSelector((state) => state.dropdown.selectedOptions);
+    const tabs = useSelector((state) => state.tabs.value);
+    const sortBy = useSelector((state) => state.sortBy.value);
+    const saves = useSelector((state) => state.saves.saves);
 
-  useEffect(() => {
-    dispatch(fetchFiles());
-  }, []);
+    useEffect(() => {
+        dispatch(fetchFiles());
+        dispatch(fetchSaves());
+    }, [dispatch]);
 
-  const compareFiles = (a, b) => {
-    if (sortBy === 'name') {
-      return a.filename.localeCompare(b.filename);
-    } else if (sortBy === 'created_at') {
-      return new Date(a.createdAt) - new Date(b.createdAt);
-    } else if (sortBy === 'size') {
-      return a.size - b.size;
-    }
-    return 0;
-  };
+    const compareFiles = (a, b) => {
+        switch (sortBy) {
+            case 'name':
+                return a.filename.localeCompare(b.filename);
+            case 'created_at':
+                return new Date(a.createdAt) - new Date(b.createdAt);
+            case 'size':
+                return a.size - b.size;
+            default:
+                return 0;
+        }
+    };
 
-  const filteredFiles = files
-    ?.filter((item) => {
-      const matchesSearch = search.toLowerCase() === '' || item.filename.toLowerCase().includes(search.toLowerCase());
-      const matchesFileType = selectedOptions.all || selectedOptions[item.type];
-      let condition = '';
+    const filteredFiles = (tabs !== 'Saved' ? files : saves)
+        .filter((item) => {
+            const matchesSearch = !search || item.filename.toLowerCase().includes(search.toLowerCase());
+            const matchesFileType = selectedOptions.all || selectedOptions[item.type];
+            let condition = false;
 
-      if (tabs === 'My Files') {
-        condition = item.deletedAt === null;
-      } else if (tabs === 'Trash Bin') {
-        condition = item.deletedAt !== null;
-      } else if (tabs === 'Starred') {
-        condition = item.starred === true;
-      }
-      return matchesSearch && matchesFileType && condition;
-    })
-    .sort(compareFiles);
+            switch (tabs) {
+                case 'My Files':
+                    condition = item.deletedAt === null;
+                    break;
+                case 'Trash Bin':
+                    condition = item.deletedAt !== null;
+                    break;
+                case 'Starred':
+                    condition = item.starred === true;
+                    break;
+                default:
+                    condition = true;
+                    break;
+            }
+            return matchesSearch && matchesFileType && condition;
+        })
+        .sort(compareFiles);
 
-  return (
-    <div className='pl-10 pr-7 h-[70vh] grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-8  overflow-y-scroll w-full'>
-      {filteredFiles.map((file) => {
-        return <Card file={file} key={file._id} />;
-      })}
-    </div>
-  );
+    return (
+        <div className='pl-10 pr-7 h-[70vh] grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-8 overflow-y-scroll w-full'>
+            {filteredFiles.map((file) => (
+                <Card file={file} key={file._id} />
+            ))}
+        </div>
+    );
 }
 
 export default CardContainer;
